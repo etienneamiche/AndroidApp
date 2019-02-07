2f23c42f81dd215e25b0d6212851471346567547
etienneamiche <etienne.amiche@gmail.com>
2019-02-07 18:54:22 +0100

    Api data fetch in Pojo Display result on fragment

---
+++ app/src/main/java/com/example/myapplication/Common/Common.java
@@ -0,0 +1,21 @@
+package com.example.myapplication.Common;
+
+
+import android.location.Location;
+
+public class Common {
+
+
+
+    public static final String API_KEY = "c0bd6ef0f0ce59b92a43277e9db4b463";
+    public static Location current_location=null;
+
+
+    public Common(){}
+
+    public static String fahrenheitToCelsius(double temp){
+        return ""+ ((temp-32)*(5/9));
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/Model/Clouds.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.Model;
+public class Clouds {
+
+    private Integer all;
+
+    public Integer getAll() {
+        return all;
+    }
+
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+
+}
